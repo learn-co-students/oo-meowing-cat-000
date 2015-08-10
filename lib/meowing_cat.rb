b64f184daf482129d221f70d4585e5a4e8c979df
aeremin <aeremin@maxus.ru>
2015-08-10 10:22:14 +0300

    done

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -5,6 +5,13 @@ class Cat
     puts "meow!"
   end
 
+  def get_in_box(box)
+    if @size <= box.size then 
+	  puts "Meow! I'm in the box!"
+	else
+      puts "Sad meow. I'm not in the box"
+	end  
+  end
   
 end
 
